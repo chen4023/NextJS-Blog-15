c9a8b852c18dc9a9677852a77def990f57fea188
현채은 <ads4023@naver.com>
2025-01-25 20:49:44 +0900

    ♻️[refactor] Link 코드 형식 변경

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,20 @@
 import Profile from '@/components/Profile';
 import React from 'react';
-const TITLE_CLASS = "text-2xl font-bold pb-3";
+const TITLE_CLASS = "text-2xl font-extrabold pb-3";
 export default function AboutPage() {
   return (
-    <div className='h-full'>
+    <>
       <Profile />
-      <section className='flex flex-col justify-center gap-16 shadow-lg mt-6 text-center h-[50vh] bg-gray-100'>
+      <section className='flex flex-col gap-6 shadow-lg m-8 p-8 text-center bg-gray-100'>
         <div>
           <h2 className={TITLE_CLASS}>👩🏻‍💻 Who am I ?</h2>
-          <p>개발을 사랑하는 프론트엔드 개발자</p>
-          <p>사람과 디자인을 담는 앱웹을 만들고 있습니다</p>
+          <p>개발을 사랑하는 프론트엔드 개발자<br />
+            사람과 디자인을 담는 앱웹을 만들고 있습니다</p>
         </div>
         <div>
           <h2 className={TITLE_CLASS}>🏢 Career</h2>
-          <p>경력입니다. 경력입니다. 경력입니다.</p>
-          <p>경력입니다. 경력입니다. 경력입니다.</p>
+          <p>경력입니다. 경력입니다. 경력입니다.<br />
+            경력입니다. 경력입니다. 경력입니다.</p>
         </div>
         <div>
           <h2 className={TITLE_CLASS}>🔨 Skills</h2>
@@ -25,7 +25,7 @@ export default function AboutPage() {
           <a href='https://github.com/chen4023'>github.com/chen4023</a>
         </div>
       </section>
-    </div>
+    </>
   );
 }
 
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,23 @@
 import { SiVelog } from "react-icons/si";
 import { FaGithub } from "react-icons/fa6";
 import ContactForm from "@/components/ContactForm";
-import Link from "next/link";
+
+const LINKS = [
+  { icon: <SiVelog />, url: 'https://velog.io/@chen4023/posts' },
+  { icon: <FaGithub />, url: 'https://github.com/chen4023' }
+]
 
 export default function ContactPage() {
-
   return (
-    <div className="w-full flex flex-col justify-center items-center gap-3 p-5">
+    <section className="w-full flex flex-col items-center gap-3 p-5">
       <h2 className="text-4xl font-extrabold">ContactPage</h2>
       <p>ads4023@naver.com</p>
       <div className="flex text-3xl gap-3">
-        <Link href='https://velog.io/@chen4023/posts'><SiVelog /></Link>
-        <Link href='https://github.com/chen4023'><FaGithub /></Link>
+        {LINKS.map((link, index) => <a key={index} href={link.url}>{link.icon}</a>)}
       </div>
       <p className="text-2xl font-extrabold">OR Send me an email 💌</p>
       <ContactForm />
-    </div>
+    </section>
   );
 }
 
